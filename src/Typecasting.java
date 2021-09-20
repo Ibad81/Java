6548f37942af02b079ef6f43fcf5fe6119b69b16
Java Basic <ibadbaig921@gmai.com>
2021-09-20 13:04:03 -0700

    Initial commit

---
+++ src/Typecasting.java
@@ -0,0 +1,26 @@
+//Type casting
+//two type
+//1). Widening Typecasting    byte -> short -> char -> int -> long -> float -> double
+//2).Narrow Typecasting       double -> float -> long -> int -> char -> short -> byte
+
+//Widening casting
+//public class Typecasting {
+//    public static void main(String[] args) {
+//        int num=21;
+//        double numd= num;
+//        System.out.println(num);
+//        System.out.println(numd);
+//    }
+//}
+
+
+public class Typecasting {
+    public static void main(String[] args) {
+        double num=21;
+        byte numi = (byte) num;
+        System.out.println(num);
+        System.out.println(numi);
+
+
+    }
+}
