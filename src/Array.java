be6bcd8c4d7f0ceb7503f0e3699e2d6dcf7f7fd8
Java Basic <ibadbaig921@gmai.com>
2021-09-20 13:39:49 -0700

    Array java

---
+++ src/Array.java
@@ -0,0 +1,48 @@
+//Java array
+//Array are used to stored the multiple values in the single variables
+
+//Java Array
+//String[] arr={"Hello","World","java"};
+//int [] arr= {1,2,3,4}
+
+
+//Access The element
+//System.out.println(arr[0]);
+
+//Change an array Element
+//arr[0]=Laptop
+
+//Array length
+//arr.length
+//sout(arr.length);
+
+
+//Loops through an array
+//for(int i = 0; i < arr.length; i++)
+
+//Accesing Each Element
+//for (String i:arr){
+//        System.out.println(i);
+
+
+public class Array {
+    public static void main(String[] args) {
+        String [] arr ={"Hello","World","Java"};               //Create an array
+//        int []arr={1,2,3,4,5};
+//        System.out.println(arr[0]);                       // Access the Element
+//        arr[0]="Laptop";                                 //   Change an array Element
+//        System.out.println(arr[0]);
+//        System.out.println(arr.length);                    //length
+
+//        for (int i = 0; i < arr.length; i++) {                      //Loop Through an array
+//            System.out.println(arr[i]);
+
+        for (String i:arr){
+            System.out.println(i);                             //Aceesing Each element
+        }
+
+
+
+
+    }
+}
